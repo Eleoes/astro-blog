4561f21a8a8a8599e8d65863a628e5ba41c8e261
eleoes <leo.escaroz@gmail.com>
2023-01-07 15:00:10 -0800

    setup astro

---
+++ astro.config.mjs
@@ -0,0 +1,4 @@
+import { defineConfig } from 'astro/config';
+// manually import and configure UI framework components such as React, Svelte, etc., Tailwind here.
+// https://astro.build/config
+export default defineConfig({});
