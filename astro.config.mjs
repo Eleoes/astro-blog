6230c17bb07105f89c7de46cc0627d2f4e712caf
eleoes <leo.escaroz@gmail.com>
2023-01-08 17:13:26 -0800

    themeIcon component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 // manually import and configure UI framework components such as React, Svelte, etc., Tailwind here.
 // https://astro.build/config
-export default defineConfig({});
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact()]
+});
